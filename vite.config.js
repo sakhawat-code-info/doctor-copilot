b21cc592ba7c4e7fe4dc585416506608173f5293
mir-sakhawat <sakhawat.code.info@gmail.com>
2025-03-04 10:31:15 +0600

    reload problem solved

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import spaFallback from "vite-plugin-spa-fallback";
-
-
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), spaFallback()],
+  plugins: [react()],
 })
