f4937a6c24f28534b1da0cbbcc38f247bad8d3bc
mir-sakhawat <sakhawat.code.info@gmail.com>
2025-03-05 20:54:11 +0600

    file added

--- src/Layout/Root.jsx
+++ src/Layout/Root.jsx
@@ -1,18 +1,18 @@
-// import Navbar from "../Components/Navbar";
-// // import Footer from "../Components/Footer";
+import Navbar from "../Components/Navbar";
+// import Footer from "../Components/Footer";
 // import { Outlet } from "react-router-dom";
 
-// const Root = () => {
-//   return (
-//     <div className=" max-w-[1920px] lg:w-[90%] w-[98%] mx-auto font-mont ">
-//       <Navbar />
+const Root = () => {
+  return (
+    <div className=" max-w-[1920px] lg:w-[90%] w-[98%] mx-auto font-mont ">
+      <Navbar />
 
-//       <div className="min-h-[calc(100vh-520.667px)]">
-//         <Outlet></Outlet>
-//       </div>
-//       {/* <Footer /> */}
-//     </div>
-//   );
-// };
+      <div className="min-h-[calc(100vh-520.667px)]">
+        {/* <Outlet></Outlet> */}
+      </div>
+      {/* <Footer /> */}
+    </div>
+  );
+};
 
-// export default Root;
+export default Root;
